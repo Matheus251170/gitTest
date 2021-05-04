5e4835d2a7bb0f33ab97c194314812befce041c0
Matheus <matheus251170@outlook.com>
2021-05-04 19:27:46 -0300

    comitando terceira atividade em java

---
+++ Projeto_Java/src/ex_1/exercicio3.java
@@ -0,0 +1,23 @@
+package ex_1;
+
+import java.util.*;
+
+public class exercicio3 {
+
+	public static void main(String[] args) {
+		
+		Scanner ler = new Scanner(System.in);
+		int tempoSegundos, horas, minutos, segundos;
+		
+		
+		System.out.print("Insira o tempo do evento em segundos: ");
+		tempoSegundos = ler.nextInt();
+		
+		horas = tempoSegundos / 3600;
+		minutos = (tempoSegundos % 3600) / 60;
+		segundos = (tempoSegundos % 3600) % 60;
+		
+		System.out.println("O evento durou " + horas + " horas, " + minutos + " minutos e " + segundos + " segundos!" );
+	}
+
+}
