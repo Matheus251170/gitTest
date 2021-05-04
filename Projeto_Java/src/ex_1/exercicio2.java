68f5caeb92f343d288384a7210e09f25ad335586
Matheus <matheus251170@outlook.com>
2021-05-04 19:22:02 -0300

    comitando segunda atividade em java

---
+++ Projeto_Java/src/ex_1/exercicio2.java
@@ -0,0 +1,26 @@
+package ex_1;
+
+import java.util.*;
+
+public class exercicio2 {
+
+	public static void main(String[] args) {
+		
+		Scanner read = new Scanner(System.in);
+		int idadeDias, anos, meses, dias;
+		
+		
+		System.out.print("Entre com sua idade em dias: ");
+		idadeDias = read.nextInt();
+		
+		anos = idadeDias / 365;
+		meses = (idadeDias % 365) / 30;
+		dias = (idadeDias % 365) % 30;
+		
+		System.out.println("Sua idade é de " + anos + " anos!");
+		System.out.println("Você nasceu no mês " + meses + "!");
+		System.out.println("Você nasceu no dia " + dias + "!");
+
+	}
+
+}
