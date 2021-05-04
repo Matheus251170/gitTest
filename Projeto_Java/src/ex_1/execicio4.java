aa824c1ec6d53e407524cf921cd75201e202d9a4
Matheus <matheus251170@outlook.com>
2021-05-04 20:28:02 -0300

    comitando quarta atividade em java

---
+++ Projeto_Java/src/ex_1/execicio4.java
@@ -0,0 +1,37 @@
+package ex_1;
+
+
+import java.util.*;
+
+
+public class execicio4 {
+
+	public static void main(String[] args) {
+		
+		Scanner read = new Scanner(System.in);
+		double a, b, c, d, r, s;
+		
+		System.out.print("Entre com o valor de A: ");
+		a = read.nextDouble();
+		
+		System.out.print("Entre com o valor de B: ");
+		b = read.nextDouble();
+		
+		System.out.print("Entre com o valor de C: ");
+		c = read.nextDouble();
+		
+		r = Math.pow((a + b), 2);
+		
+		s = Math.pow((b + c), 2);
+		
+		d = (r + s) / 2;
+		
+		System.out.println("O valor de R é " + r);
+		System.out.println("O valor de S é " + s);
+		System.out.printf("O valor de D é %.2f", d);
+		
+
+
+	}
+
+}
