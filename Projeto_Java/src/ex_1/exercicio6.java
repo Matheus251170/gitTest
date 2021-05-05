54e64b228b92be6598c6ca581a60a0cdd51b04c4
Matheus <matheus251170@outlook.com>
2021-05-04 21:32:12 -0300

    comitando o sexto exercicio em java

---
+++ Projeto_Java/src/ex_1/exercicio6.java
@@ -0,0 +1,33 @@
+package ex_1;
+
+import java.util.*;
+
+public class exercicio6 {
+
+	public static void main(String[] args) {
+
+		Scanner read = new Scanner(System.in);
+		double x1, x2, y1, y2, distancia, raizDist;
+		
+		System.out.print("Entre com o valor de x1: ");
+		x1 = read.nextDouble();
+		
+		System.out.print("Entre com o valor de x2: ");
+		x2 = read.nextDouble();
+		
+		System.out.print("Entre com o valor de y1: ");
+		y1 = read.nextDouble();
+		
+		System.out.print("Entre com o valor de y2: ");
+		y2 = read.nextDouble();
+		
+		
+		distancia = Math.pow((x2 - x1), 2) + Math.pow((y2 + y1), 2);
+		raizDist = Math.sqrt(2);
+		
+		
+		System.out.printf("A distância entre os pontos é: %.2f", raizDist);
+
+	}
+
+}
