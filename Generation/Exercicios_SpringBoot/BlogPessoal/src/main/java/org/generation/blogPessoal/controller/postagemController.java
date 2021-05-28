f2f14e489c52fb8d72821d6be40ab223b44c2bc1
Matheus <matheus251170@outlook.com>
2021-05-28 09:53:44 -0300

    adiciona metodos put e delete

--- Generation/Exercicios_SpringBoot/BlogPessoal/src/main/java/org/generation/blogPessoal/controller/postagemController.java
+++ Generation/Exercicios_SpringBoot/BlogPessoal/src/main/java/org/generation/blogPessoal/controller/postagemController.java
@@ -4,10 +4,15 @@ import org.generation.blogPessoal.model.postagem;
 import java.util.List;
 import org.generation.blogPessoal.reposiroy.PostagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,4 +41,24 @@ public class postagemController {
 	{
 		return ResponseEntity.ok(repo.findAllByTituloContainingIgnoreCase(titulo));
 	}
+	
+	@PostMapping
+	public ResponseEntity<postagem> post (@RequestBody postagem novoPost)
+	{
+		return ResponseEntity.status(HttpStatus.CREATED).body(repo.save(novoPost));
+	}
+	
+	@PutMapping
+	public ResponseEntity<postagem> put (@RequestBody postagem novoPost)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(repo.save(novoPost));
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable long id)
+	{
+		repo.deleteById(id);
+	}
+	
+	
 }
